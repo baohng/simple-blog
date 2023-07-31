14e7951d6c171f35031bcb0fa732e457a43311aa
baohng <bao.hng221b@gmail.com>
2023-07-31 13:34:24 +0700

    add route to contact page and pass data to it

--- app.js
+++ app.js
@@ -21,7 +21,11 @@ app.get('/about', (req, res) => {
   });
 });
 
-
+app.get('/contact', (req, res) => {
+  res.render("contact.ejs", {
+    contactContent: contactContent,
+  });
+});
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
